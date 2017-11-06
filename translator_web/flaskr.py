988706f68bdd69ff14dfb3f7907d75afd0c53efb
xschen <xs0040@gmail.com>
2017-11-06 12:24:04 +0800

    web api

--- translator_web/flaskr.py
+++ translator_web/flaskr.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, send_from_directory, redirect, render_template, flash, url_for
+from flask import Flask, request, send_from_directory, redirect, render_template, flash, url_for, jsonify, \
+    make_response, abort
 from translator_web.eng_to_fra_char_translator_predict import EngToFraCharTranslator
 from translator_web.eng_to_cmn_char_translator_predict import EngToCmnCharTranslator
 from translator_web.eng_to_fra_word_translator_predict import EngToFraWordTranslator
@@ -22,6 +23,7 @@ eng_to_fra_translator_w.test_run()
 
 eng_to_cmn_translator_w = EngToCmnWordTranslator()
 eng_to_cmn_translator_w.test_run()
+
 
 @app.route('/')
 def home():
@@ -98,6 +100,41 @@ def eng_to_cmn_word_translator():
             return render_template('eng_to_cmn_word_translator_result.html', sentence=sent,
                                    sentiments=sentiments)
     return render_template('eng_to_cmn_word_translator.html')
+
+
+@app.route('/translate_eng', method=['POST', 'GET'])
+def translate_eng():
+    if request.method == 'POST':
+        if not request.json or 'sentence' not in request.json or 'level' not in request.json or 'target_lang' not in request.json:
+            abort(400)
+        sentence = request.json['sentence']
+        level = request.json['level']
+        target_lang = request.json['target_lang']
+    else:
+        sentence = request.args.get('sentence')
+        level = request.args.get('level')
+        target_lang = request.args.get('target_lang')
+
+    target_text = sentence
+    if level == 'char' and target_lang == 'french':
+        target_text = eng_to_fra_translator_c.translate_lang(sentence)
+    elif level == 'char' and target_lang == 'chinese':
+        target_text = eng_to_cmn_translator_c.translate_lang(sentence)
+    elif level == 'word' and target_lang == 'french':
+        target_text = eng_to_fra_translator_w.translate_lang(sentence)
+    elif level == 'word' and target_lang == 'chinese':
+        target_text = eng_to_cmn_translator_w.translate_lang(sentence)
+    return jsonify({
+        'sentence': sentence,
+        'translated': target_text,
+        'target_lang': target_lang,
+        'level': level
+    })
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == '__main__':
