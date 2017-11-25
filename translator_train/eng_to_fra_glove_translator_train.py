2c41be0fb30a962386a9994589bb7da427829a1d
xschen <xs0040@gmail.com>
2017-11-25 17:32:19 +0800

    eng to fra glove encoding

---
+++ translator_train/eng_to_fra_glove_translator_train.py
@@ -0,0 +1,180 @@
+from keras.models import Model
+from keras.layers.recurrent import LSTM
+from keras.layers import Dense, Input, Embedding
+from keras.preprocessing.sequence import pad_sequences
+from collections import Counter
+import nltk
+import numpy as np
+import os
+import sys
+import zipfile
+import urllib.request
+
+BATCH_SIZE = 64
+NUM_EPOCHS = 100
+HIDDEN_UNITS = 256
+NUM_SAMPLES = 10000
+MAX_VOCAB_SIZE = 10000
+GLOVE_EMBEDDING_SIZE = 100
+DATA_PATH = 'data/fra.txt'
+
+target_counter = Counter()
+
+GLOVE_MODEL = "very_large_data/glove.6B." + str(GLOVE_EMBEDDING_SIZE) + "d.txt"
+WHITELIST = 'abcdefghijklmnopqrstuvwxyz1234567890?.,'
+
+
+def in_white_list(_word):
+    for char in _word:
+        if char in WHITELIST:
+            return True
+
+    return False
+
+
+def reporthook(block_num, block_size, total_size):
+    read_so_far = block_num * block_size
+    if total_size > 0:
+        percent = read_so_far * 1e2 / total_size
+        s = "\r%5.1f%% %*d / %d" % (
+            percent, len(str(total_size)), read_so_far, total_size)
+        sys.stderr.write(s)
+        if read_so_far >= total_size:  # near the end
+            sys.stderr.write("\n")
+    else:  # total size is unknown
+        sys.stderr.write("read %d\n" % (read_so_far,))
+
+
+def download_glove():
+    if not os.path.exists(GLOVE_MODEL):
+
+        glove_zip = 'very_large_data/glove.6B.zip'
+
+        if not os.path.exists('very_large_data'):
+            os.makedirs('very_large_data')
+
+        if not os.path.exists(glove_zip):
+            print('glove file does not exist, downloading from internet')
+            urllib.request.urlretrieve(url='http://nlp.stanford.edu/data/glove.6B.zip', filename=glove_zip,
+                                       reporthook=reporthook)
+
+        print('unzipping glove file')
+        zip_ref = zipfile.ZipFile(glove_zip, 'r')
+        zip_ref.extractall('very_large_data')
+        zip_ref.close()
+
+
+def load_glove():
+    download_glove()
+    _word2em = {}
+    file = open(GLOVE_MODEL, mode='rt', encoding='utf8')
+    for line in file:
+        words = line.strip().split()
+        word = words[0]
+        embeds = np.array(words[1:], dtype=np.float32)
+        _word2em[word] = embeds
+    file.close()
+    return _word2em
+
+word2em = load_glove()
+
+lines = open(DATA_PATH, 'rt', encoding='utf8').read().split('\n')
+for line in lines[: min(NUM_SAMPLES, len(lines)-1)]:
+    input_text, target_text = line.split('\t')
+    input_words = [w for w in nltk.word_tokenize(input_text.lower())]
+    target_text = 'START ' + target_text.lower() + ' END'
+    target_words = [w for w in nltk.word_tokenize(target_text)]
+    for w in target_words:
+        target_counter[w] += 1
+
+target_word2idx = dict()
+for idx, word in enumerate(target_counter.most_common(MAX_VOCAB_SIZE)):
+    target_word2idx[word[0]] = idx + 1
+
+target_word2idx['UNK'] = 0
+
+target_idx2word = dict([(idx, word) for word, idx in target_word2idx.items()])
+
+num_decoder_tokens = len(target_idx2word)
+
+np.save('models/eng-to-fra/eng-to-fra-glove-target-word2idx.npy', target_word2idx)
+np.save('models/eng-to-fra/eng-to-fra-glove-target-idx2word.npy', target_idx2word)
+
+unknown_emb = np.random.randn(GLOVE_EMBEDDING_SIZE)
+
+np.save('models/eng-to-fra/eng-to-fra-glove-unknown-emb', unknown_emb)
+
+encoder_input_data = []
+
+encoder_max_seq_length = 0
+decoder_max_seq_length = 0
+
+lines = open(DATA_PATH, 'rt', encoding='utf8').read().split('\n')
+for line in lines[: min(NUM_SAMPLES, len(lines)-1)]:
+    input_text, target_text = line.split('\t')
+    target_text = 'START ' + target_text.lower() + ' END'
+    input_words = [w for w in nltk.word_tokenize(input_text.lower())]
+    target_words = [w for w in nltk.word_tokenize(target_text)]
+    encoder_input_emb = []
+    for w in input_words:
+        emb = unknown_emb
+        if w in word2em:
+            emb = word2em[w]
+        encoder_input_emb.append(emb)
+
+    encoder_input_data.append(encoder_input_emb)
+    encoder_max_seq_length = max(len(encoder_input_emb), encoder_max_seq_length)
+    decoder_max_seq_length = max(len(target_words), decoder_max_seq_length)
+
+encoder_input_data = pad_sequences(encoder_input_data, encoder_max_seq_length)
+
+decoder_target_data = np.zeros(shape=(NUM_SAMPLES, decoder_max_seq_length, num_decoder_tokens))
+decoder_input_data = np.zeros(shape=(NUM_SAMPLES, decoder_max_seq_length, num_decoder_tokens))
+lines = open(DATA_PATH, 'rt', encoding='utf8').read().split('\n')
+for lineIdx, line in enumerate(lines[: min(NUM_SAMPLES, len(lines)-1)]):
+    _, target_text = line.split('\t')
+    target_text = 'START ' + target_text.lower() + ' END'
+    target_words = [w for w in nltk.word_tokenize(target_text)]
+    for idx, w in enumerate(target_words):
+        w2idx = 0  # default [UNK]
+        if w in target_word2idx:
+            w2idx = target_word2idx[w]
+        decoder_input_data[lineIdx, idx, w2idx] = 1
+        if idx > 0:
+            decoder_target_data[lineIdx, idx-1, w2idx] = 1
+
+context = dict()
+context['num_decoder_tokens'] = num_decoder_tokens
+context['encoder_max_seq_length'] = encoder_max_seq_length
+context['decoder_max_seq_length'] = decoder_max_seq_length
+
+np.save('models/eng-to-fra/eng-to-fra-glove-context.npy', context)
+
+encoder_inputs = Input(shape=(None, GLOVE_EMBEDDING_SIZE), name='encoder_inputs')
+encoder_lstm = LSTM(units=HIDDEN_UNITS, return_state=True, name='encoder_lstm')
+encoder_outputs, encoder_state_h, encoder_state_c = encoder_lstm(encoder_inputs)
+encoder_states = [encoder_state_h, encoder_state_c]
+
+decoder_inputs = Input(shape=(None, num_decoder_tokens), name='decoder_inputs')
+decoder_lstm = LSTM(units=HIDDEN_UNITS, return_state=True, return_sequences=True, name='decoder_lstm')
+decoder_outputs, decoder_state_h, decoder_state_c = decoder_lstm(decoder_inputs,
+                                                                 initial_state=encoder_states)
+decoder_dense = Dense(units=num_decoder_tokens, activation='softmax', name='decoder_dense')
+decoder_outputs = decoder_dense(decoder_outputs)
+
+model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+model.fit([encoder_input_data, decoder_input_data], decoder_target_data, batch_size=BATCH_SIZE, epochs=NUM_EPOCHS,
+          verbose=1, validation_split=0.2)
+
+json = model.to_json()
+open('models/eng-to-fra/eng-to-fra-glove-architecture.json', 'w').write(json)
+model.save_weights('models/eng-to-fra/eng-to-fra-glove-weights.h5')
+
+
+
+
+
+
+
---
+++ translator_web/eng_to_fra_glove_translator_predict.py
@@ -0,0 +1,168 @@
+from keras.models import Model, model_from_json
+from keras.layers import Input, LSTM, Dense, Embedding
+from keras.preprocessing.sequence import pad_sequences
+import numpy as np
+import nltk
+import os
+import sys
+import urllib.request
+import zipfile
+
+HIDDEN_UNITS = 256
+GLOVE_EMBEDDING_SIZE = 100
+
+GLOVE_MODEL = "very_large_data/glove.6B." + str(GLOVE_EMBEDDING_SIZE) + "d.txt"
+WHITELIST = 'abcdefghijklmnopqrstuvwxyz1234567890?.,'
+
+
+def in_white_list(_word):
+    for char in _word:
+        if char in WHITELIST:
+            return True
+
+    return False
+
+
+def reporthook(block_num, block_size, total_size):
+    read_so_far = block_num * block_size
+    if total_size > 0:
+        percent = read_so_far * 1e2 / total_size
+        s = "\r%5.1f%% %*d / %d" % (
+            percent, len(str(total_size)), read_so_far, total_size)
+        sys.stderr.write(s)
+        if read_so_far >= total_size:  # near the end
+            sys.stderr.write("\n")
+    else:  # total size is unknown
+        sys.stderr.write("read %d\n" % (read_so_far,))
+
+
+def download_glove():
+    if not os.path.exists(GLOVE_MODEL):
+
+        glove_zip = 'very_large_data/glove.6B.zip'
+
+        if not os.path.exists('very_large_data'):
+            os.makedirs('very_large_data')
+
+        if not os.path.exists(glove_zip):
+            print('glove file does not exist, downloading from internet')
+            urllib.request.urlretrieve(url='http://nlp.stanford.edu/data/glove.6B.zip', filename=glove_zip,
+                                       reporthook=reporthook)
+
+        print('unzipping glove file')
+        zip_ref = zipfile.ZipFile(glove_zip, 'r')
+        zip_ref.extractall('very_large_data')
+        zip_ref.close()
+
+
+def load_glove():
+    download_glove()
+    _word2em = {}
+    file = open(GLOVE_MODEL, mode='rt', encoding='utf8')
+    for line in file:
+        words = line.strip().split()
+        word = words[0]
+        embeds = np.array(words[1:], dtype=np.float32)
+        _word2em[word] = embeds
+    file.close()
+    return _word2em
+
+
+class EngToFraGloveTranslator(object):
+    model = None
+    encoder_model = None
+    decoder_model = None
+    target_word2idx = None
+    target_idx2word = None
+    max_encoder_seq_length = None
+    max_decoder_seq_length = None
+    num_decoder_tokens = None
+    word2em = None
+    unknown_emb = None
+
+    def __init__(self):
+        self.word2em = load_glove()
+        self.target_word2idx = np.load(
+            '../translator_train/models/eng-to-fra/eng-to-fra-word-target-word2idx.npy').item()
+        self.target_idx2word = np.load(
+            '../translator_train/models/eng-to-fra/eng-to-fra-word-target-idx2word.npy').item()
+        context = np.load('../translator_train/models/eng-to-fra/eng-to-fra-word-context.npy').item()
+        print(context)
+        self.max_encoder_seq_length = context['encoder_max_seq_length']
+        self.max_decoder_seq_length = context['decoder_max_seq_length']
+        self.num_decoder_tokens = context['num_decoder_tokens']
+
+        encoder_inputs = Input(shape=(None, GLOVE_EMBEDDING_SIZE), name='encoder_inputs')
+        encoder_lstm = LSTM(units=HIDDEN_UNITS, return_state=True, name="encoder_lstm")
+        encoder_outputs, encoder_state_h, encoder_state_c = encoder_lstm(encoder_inputs)
+        encoder_states = [encoder_state_h, encoder_state_c]
+
+        decoder_inputs = Input(shape=(None, self.num_decoder_tokens), name='decoder_inputs')
+        decoder_lstm = LSTM(units=HIDDEN_UNITS, return_sequences=True, return_state=True, name='decoder_lstm')
+        decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
+        decoder_dense = Dense(self.num_decoder_tokens, activation='softmax', name='decoder_dense')
+        decoder_outputs = decoder_dense(decoder_outputs)
+
+        self.model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+
+        # model_json = open('../translator_train/models/eng-to-fra/eng-to-fra-word-architecture.json', 'r').read()
+        # self.model = model_from_json(model_json)
+        self.model.load_weights('../translator_train/models/eng-to-fra/eng-to-fra-word-weights.h5')
+        self.model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+
+        self.encoder_model = Model(encoder_inputs, encoder_states)
+
+        decoder_state_inputs = [Input(shape=(HIDDEN_UNITS,)), Input(shape=(HIDDEN_UNITS,))]
+        decoder_outputs, state_h, state_c = decoder_lstm(decoder_inputs, initial_state=decoder_state_inputs)
+        decoder_states = [state_h, state_c]
+        decoder_outputs = decoder_dense(decoder_outputs)
+        self.decoder_model = Model([decoder_inputs] + decoder_state_inputs, [decoder_outputs] + decoder_states)
+
+    def translate_lang(self, input_text):
+        input_seq = []
+        input_wids = []
+        for word in nltk.word_tokenize(input_text.lower()):
+            idx = self.unknown_emb
+            if word in self.word2em:
+                idx = self.word2em[word]
+            input_wids.append(idx)
+        input_seq.append(input_wids)
+        input_seq = pad_sequences(input_seq, self.max_encoder_seq_length)
+        states_value = self.encoder_model.predict(input_seq)
+        target_seq = np.zeros((1, 1, self.num_decoder_tokens))
+        target_seq[0, 0, self.target_word2idx['START']] = 1
+        target_text = ''
+        terminated = False
+        target_text_len = 0
+        while not terminated:
+            output_tokens, h, c = self.decoder_model.predict([target_seq] + states_value)
+
+            sample_token_idx = np.argmax(output_tokens[0, -1, :])
+            sample_word = self.target_idx2word[sample_token_idx]
+            target_text_len += 1
+
+            if sample_word != 'START' and sample_word != 'END':
+                target_text += ' ' + sample_word
+
+            if sample_word == 'END' or target_text_len >= self.max_decoder_seq_length:
+                terminated = True
+
+            target_seq = np.zeros((1, 1, self.num_decoder_tokens))
+            target_seq[0, 0, sample_token_idx] = 1
+            states_value = [h, c]
+
+        return target_text.strip()
+
+    def test_run(self):
+        print(self.translate_lang('Be nice.'))
+        print(self.translate_lang('Drop it!'))
+        print(self.translate_lang('Get out!'))
+
+
+def main():
+    model = EngToFraGloveTranslator()
+    model.test_run()
+
+
+if __name__ == '__main__':
+    main()
